da1ae886701d51dd0b49fc5be533fb7c531399e0
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-07-16 13:46:16 +0300

    Use generic with states

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface Props {
+  name: string;
+}
+
+const HelloWorld: React.FC<Props> = ({ name }) => {
+  const [state] = useState<{ fullName: string | null }>({ fullName: '' });
+  // state.fullName
+
+  return <div>Hello {name}</div>;
+};
 
 const App: React.FC = () => {
   return <div>Hello</div>;
